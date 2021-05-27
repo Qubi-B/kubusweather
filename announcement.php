bcc85e6dfb33854e0bb63d4999ff3b944dce4bac
QubiB <61910780+Qubi-B@users.noreply.github.com>
2021-05-27 10:29:47 +0200

    Create announcement.php

---
+++ announcement.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Announcement</title>
+    <link rel="stylesheet" href="/clouds/announcement.css">
+  </head>
+  <body>
+    <section class="main_box">
+          <table>
+            <tr>
+              <td><img src="promotion.png" alt="obrazek" class="annoicon"></td>
+              <td><h1>Uwaga!</h1></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>Nie bierz aktualnie pod uwagę danych o jakości powietrza z tej strony, bo trwa kalibracja czujnika.<br><br></td>
+            </tr>
+          </table>
+    </section>
+    <!--
+  </body>
+</html>
